d311e7408901ea59a85b7c5ba468c6ee849a95b1
Jonty Dawson <jonty.dawson@eegeo.com>
2018-11-09 18:19:43 +0000

    add position field to IndoorMapEntity
    Buddy: Micky

--- sdk/src/main/java/com/eegeo/mapapi/indoorentities/IndoorMapEntity.java
+++ sdk/src/main/java/com/eegeo/mapapi/indoorentities/IndoorMapEntity.java
@@ -3,6 +3,8 @@ package com.eegeo.mapapi.indoorentities;
 import android.support.annotation.NonNull;
 import android.support.annotation.UiThread;
 
+import com.eegeo.mapapi.geometry.LatLng;
+
 /**
  * Represents infomation about an identifiable feature on an indoor map.
  * These correspond to features within a level GeoJSON in an indoor map submission via the WRLD Indoor Map REST API.
@@ -23,15 +25,26 @@ public class IndoorMapEntity
     public final int indoorMapFloorId;
 
     /**
+     * The location of this indoor map entity. Although indoor map entities can represent area
+     * features such as rooms or desks, this position provides a point that is in the center of the
+     * feature. As such, it is suitable for use if locating a Marker for this entity, or if
+     * positioning the camera to look at this entity.
+     */
+    public final LatLng position;
+
+    /**
      * @eegeo.internal
      */
     @UiThread
     public IndoorMapEntity(
             @NonNull String indoorMapEntityId,
-            @NonNull int indoorMapFloorId
+            @NonNull int indoorMapFloorId,
+            @NonNull LatLng position
+
     ) {
         this.indoorMapEntityId = indoorMapEntityId;
         this.indoorMapFloorId = indoorMapFloorId;
+        this.position = position;
     }
 
 }
